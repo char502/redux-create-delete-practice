a8b927efd3359fb060f2614ef320f884d1400d70
char502 <char502@hotmail.com>
2021-01-14 22:09:42 +0000

    setting up redux, connecting to app.js

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -4,6 +4,8 @@ export default (state = [], action) => {
   switch (action.actionTypes) {
     case actionTypes.CREATE_NEW_CONTACT:
       return [...state, Object.assign({}, action.contact)];
+    case actionTypes.REMOVE_CONTACT:
+      return state.filter((data, i) => i !== action.id);
     default:
       return state;
   }
